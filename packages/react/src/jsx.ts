6127ffaa896d339b961fb421aca775d7beee3840
Amir Yue Zhou <amirzhou001@gmail.com>
2025-01-24 11:36:16 -0700

    feat: define ReactElement constructor

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,1 +1,12 @@
 // return ReactElement
+
+const ReactElement = function (type, key, ref, props) {
+	const element = {
+		$$typeof: xxx,
+		key,
+		ref,
+		props,
+		__mark: 'amir'
+	};
+	return element;
+};
